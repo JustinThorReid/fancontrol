1810882a6819c3e079e3379ba7f96dd2484b85fa
Justin Thor Reid <JustinThorReid@users.noreply.github.com>
2021-08-19 18:29:36 -0400

    Add OTA and MQTT as submodules

--- fan-controller.ino
+++ fan-controller.ino
@@ -2,7 +2,7 @@
 #include "secrets.h"
 #include "data.h"
 
-#include "mqtt/MQTTHelper.h"
+#include "arduino-mqtt-helper/MQTTHelper.h"
 #include "updater/Updater.h"
 
 MQTTHelper *mqtt;
@@ -14,66 +14,63 @@ const uint16_t PULSE_WIDTH = 400;
 void setup()
 {
   pinMode(TX_PIN, OUTPUT);
-
-  Serial.begin(115200);
-  Serial.println("Start");
-
-  mqtt = MQTTHelper::getInstance();
-  mqtt->startConnection(WIFI_SSID, WIFI_PASS, MQTT_HOST, MQTT_USER, MQTT_PASS, MQTT_CLIENT);
-
-  mqtt->subscribe(BEDROOM_FAN, MQTTQOS::EXACTLY_ONCE);
-  mqtt->subscribe(BEDROOM_LIGHT, MQTTQOS::EXACTLY_ONCE);
-  mqtt->subscribe(OFFICE_FAN, MQTTQOS::EXACTLY_ONCE);
-  mqtt->subscribe(OFFICE_LIGHT, MQTTQOS::EXACTLY_ONCE);
-
-  updater = Updater::getInstance();
-
-  transmit(400, lowSpeed, sizeof(lowSpeed));
-  transmit(400, lowSpeed, sizeof(lowSpeed));
+  transmit(DATA_PULSE_WIDTH, officeLight, sizeof(officeLight));
+//  Serial.begin(115200);
+//  Serial.println("Start");
+//
+//  mqtt = MQTTHelper::getInstance();
+//  mqtt->startConnection(WIFI_SSID, WIFI_PASS, MQTT_HOST, MQTT_USER, MQTT_PASS, MQTT_CLIENT);
+//
+//  mqtt->subscribe(BEDROOM_FAN, MQTTQOS::EXACTLY_ONCE);
+//  mqtt->subscribe(BEDROOM_LIGHT, MQTTQOS::EXACTLY_ONCE);
+//  mqtt->subscribe(OFFICE_FAN, MQTTQOS::EXACTLY_ONCE);
+//  mqtt->subscribe(OFFICE_LIGHT, MQTTQOS::EXACTLY_ONCE);
+//
+//  updater = Updater::getInstance();
 }
 
 void loop()
 {
-  updater->loop();
-
-  MQTTNotification *notification = mqtt->loop();
-  if (notification != nullptr)
-  {
-    boolean shouldEnable = strcmp(notification->payload, "true");
-
-    switch (notification->payload)
-    {
-      // Bedroom fan doesn't have a off command
-    case BEDROOM_FAN:
-      transmit(DATA_PULSE_WIDTH, bedFan, sizeof(bedFan));
-      transmit(DATA_PULSE_WIDTH, bedFan, sizeof(bedFan));
-      break;
-    case BEDROOM_LIGHT:
-      transmit(DATA_PULSE_WIDTH, bedLight, sizeof(bedLight));
-      transmit(DATA_PULSE_WIDTH, bedLight, sizeof(bedLight));
-      break;
-
-    case OFFICE_FAN:
-      if (shouldEnable)
-      {
-        transmit(DATA_PULSE_WIDTH, officeLowSpeed, sizeof(officeLowSpeed));
-        transmit(DATA_PULSE_WIDTH, officeLowSpeed, sizeof(officeLowSpeed));
-      }
-      else
-      {
-        transmit(DATA_PULSE_WIDTH, officeOffSpeed, sizeof(officeOffSpeed));
-        transmit(DATA_PULSE_WIDTH, officeOffSpeed, sizeof(officeOffSpeed));
-      }
-      break;
-    case OFFICE_LIGHT:
-      transmit(DATA_PULSE_WIDTH, officeLight, sizeof(officeLight));
-      transmit(DATA_PULSE_WIDTH, officeLight, sizeof(officeLight));
-      break;
-
-    default:
-      break;
-    }
-  }
+//  updater->loop();
+//
+//  MQTTNotification *notification = mqtt->loop();
+//  if (notification != nullptr)
+//  {
+//    boolean shouldEnable = strcmp(notification->payload, "true");
+//
+//    switch (notification->payload)
+//    {
+//      // Bedroom fan doesn't have a off command
+//    case BEDROOM_FAN:
+//      transmit(DATA_PULSE_WIDTH, bedFan, sizeof(bedFan));
+//      transmit(DATA_PULSE_WIDTH, bedFan, sizeof(bedFan));
+//      break;
+//    case BEDROOM_LIGHT:
+//      transmit(DATA_PULSE_WIDTH, bedLight, sizeof(bedLight));
+//      transmit(DATA_PULSE_WIDTH, bedLight, sizeof(bedLight));
+//      break;
+//
+//    case OFFICE_FAN:
+//      if (shouldEnable)
+//      {
+//        transmit(DATA_PULSE_WIDTH, officeLowSpeed, sizeof(officeLowSpeed));
+//        transmit(DATA_PULSE_WIDTH, officeLowSpeed, sizeof(officeLowSpeed));
+//      }
+//      else
+//      {
+//        transmit(DATA_PULSE_WIDTH, officeOffSpeed, sizeof(officeOffSpeed));
+//        transmit(DATA_PULSE_WIDTH, officeOffSpeed, sizeof(officeOffSpeed));
+//      }
+//      break;
+//    case OFFICE_LIGHT:
+//      transmit(DATA_PULSE_WIDTH, officeLight, sizeof(officeLight));
+//      transmit(DATA_PULSE_WIDTH, officeLight, sizeof(officeLight));
+//      break;
+//
+//    default:
+//      break;
+//    }
+//  }
 }
 
 void transmit(uint16_t pulseWidth, uint8_t *data, uint8_t len)
